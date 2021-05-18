0709a4f1691633dd740a7767115e363bf2a8f201
LogeshRavi <rlogeshfive@gmail.com>
2021-05-18 11:53:42 +0530

    first commit

--- userRoute.js
+++ userRoute.js
@@ -201,7 +201,7 @@ router.post('/educator/register', async (req, res) => {
        console.log(err)
      }
      else{
-      res.json({ StatusCode: 200, StatusMessage: "Success", Response: "OTP Send Successfully" })
+      res.json({ StatusCode: 200, StatusMessage: "Success", Response: "OTP Send Successfully",User:data })
 
      }
   })
